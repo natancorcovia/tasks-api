ef790b27365f90bd5ea4a5f31a02e0b0367a99bc
Natan Corcovia <natankorkov@gmail.com>
2025-09-08 18:16:56 -0300

    feat: setup user.entity and user.service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entity/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+
+  async create(username: string, password: string, role: 'user' | 'admin') {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = { id: Date.now(), username, password: hashed, role };
+    this.users.push(user);
+    return user;
+  }
+
+  async findByUsername(username: string) {
+    return this.users.find((u) => u.username === username);
+  }
+
+  async findById(id: number) {
+    return this.users.find((u) => u.id === id);
+  }
+}
